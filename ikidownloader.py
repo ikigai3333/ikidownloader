a7a9804dcabc8fe2f0c3202af3faeaea1cc8e3a1
iki <83661924+ikigai3333@users.noreply.github.com>
2021-10-31 04:33:49 +0300

    Add files via upload

---
+++ ikidownloader.py
@@ -0,0 +1,23 @@
+import requests
+import os
+
+def linkeditor(a, b):
+    if b and a.endswith(b):
+        return a[:-len(b)]
+    return a
+
+c = 0
+fn = 'instagram{}.png'
+while os.path.isfile(fn.format(c)):
+    c += 1
+fn = fn.format(c)
+
+
+l = input('  >> The URL: ')
+
+v = linkeditor(l, '?utm_source=ig_web_copy_link')
+img = requests.get(f'{v}media/?size=l').content
+with open(fn, 'wb') as f:
+    f.write(img)
+
+input('  >> Downloaded successfully!')
